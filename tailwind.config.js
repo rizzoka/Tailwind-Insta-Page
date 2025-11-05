41784e01c7064ced30ba19dae95e1bd34c788cfe
RizzoKa <kauarizzo06@gmail.com>
2025-11-05 17:58:54 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/**  @type{import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{htnl,js}'],
+    theme: {
+        extend: {
+            'blue-44' : '#456456' 
+            // cores e outros ajustes personalizados podem ser adicionados aqui
+        },
+    },
+    plugins: [],
+}
